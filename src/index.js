9e6ccb329ca7e84790f12768cab107d6d899752e
frank-hz <carlos.henostroza.94@gmail.com>
2024-09-09 00:24:26 -0500

    [0.1] feat: agregada funcion de almacenar archivos usando la api de cloudinary

--- src/index.js
+++ src/index.js
@@ -1,48 +1,69 @@
 const express = require('express');
+const cloudinary = require('cloudinary');
+const path = require('path');
 const multer = require('multer');
-const path = require('path'); 
-const ejs = require('ejs')
-
-
-const storage = multer.diskStorage({
-    destination: 'src/uploads/',
-    filename: (req, file, cb)=>{
-        cb(null, Date.now() + path.extname(file.originalname));
-    }
-})
-const upload = multer({storage: storage})
 const app = express();
+require('ejs');
+require('dotenv').config();
 
-app.set('views',path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'views'));
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+// multer config
+const multerUpload = multer({storage: multer.memoryStorage()}) 
+
+// cloudinary config 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
 
-app.get('/', (req,res)=>{
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+app.post('/upload', multerUpload.single('my-file'), async(req,res)=>{
+    const file = req.file;
+    if (!file){
+        res.status(400).send('<p>Archivo no encontrado</p>');
+    }
+
+    let base_64 = Buffer.from(file.buffer).toString("base64")
+    let data_uri = "data:" + file.mimetype + ";base64," + base_64;
+    let save = await uploadImage(data_uri);
+    if (!save){
+        res.send('Error al guardar')
+        return;
+    }
+    console.log(save);
     res.send(`
-        <h1>Cloudy Uploader</h1>
-        <form action="/upload" method="POST" enctype="multipart/form-data"   autocomplete="off">
-            <label for="name">Nombre</label>
-            <input type="text" name="name"> <br>
-            <label for="image">Imagen</label>
-            <input type="file" name="image" id="image"> <br>
-            <button type="submit">Subir</button>
-        </form>
+        <p>Archivo guardado: ${save}</p>
+        <a href="/">Volver</a>
     `);
 })
 
-app.post('/upload', upload.single('image'), (req, res) =>{
-    // const {name, image} = req.body;
-    // console.log(req.body);
-    // res.send('Name: '+name)    
-    res.render('index', {
-        message: `Archivo guardado: ${req.file.filename}`
-    });
-    console.log(`Archivo guardado: ${req.file.filename}`);
+async function uploadImage(filePath_){
+    const options = {
+        use_filename: true,
+        unique_filename: false,
+        overwrite: true
+    }
+    const result = await cloudinary.uploader.upload(filePath_, options)
+    const id = result.public_id;
+
+    if (!id){
+        console.log('Error al obtener imagen');
+        return;
+    }
+    return id;
+}
+
+
+app.listen(process.env.PORT, (req,res)=>{
+    console.log(`Live at ${process.env.PORT}`);
 })
 
-app.listen('3000', (req,res) => {
-    console.log('server in 3000');
-})
+
